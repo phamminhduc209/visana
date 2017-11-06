6d3d4b83a1f7bc76953e8cca18628caa7a6c8cd2
Ducpm <minhducpham.it@gmail.com>
2017-11-06 08:10:21 +0700

    Duc

--- js/common.js
+++ js/common.js
@@ -30,7 +30,6 @@
    * 2. Sticky Menu
   */
   $('.fixed').sticky({ topSpacing: 0 });
-  $('.fixed-res').sticky({ topSpacing: 105 });
   /*
    * 3. select2
   */
@@ -39,37 +38,15 @@
   /*
    * 4. datetimepicker
   */
-  // $(".datetimepicker").datetimepicker({
-  //   format: 'MM/DD/YYYY',
-  //   useCurrent: false
-  // });
-
-  /*
-   * 5. Not Overflow datetimepicker
-  */
-  $('.date').on('dp.show', function (e) {
-    var datepicker = $('body').find('.bootstrap-datetimepicker-widget:last'),
-        position = datepicker.offset(),
-        parent = datepicker.parent(),
-        parentPos = parent.offset(),
-        width = datepicker.width(),
-        parentWid = parent.width();
-    // move datepicker to the exact same place it was but attached to body
-    datepicker.appendTo('body');
-    datepicker.css({
-        position: 'absolute',
-        top: position.top,
-        bottom: 'auto',
-        left: position.left,
-        right: 'auto',
-        'z-index': '9999'
-    });
-    // if datepicker is wider than the thing it is attached to then move it so the centers line up
-    if (parentPos.left + parentWid < position.left + width) {
-        var newLeft = parentPos.left;
-        newLeft += parentWid / 0;
-        newLeft -= width / 0;
-        datepicker.css({left: newLeft});
-    }
+  $(function(){
+    // Determine the div width parent
+    var width = document.getElementById('nav-service-wrap').offsetWidth;
+    // Determine the div width parent
+    var mainDiv = $('.nav-service');
+    var childDivCount = mainDiv.find('li').length;
+    // Get width for li
+    var widthItem = width/childDivCount;
+    $('.nav-service li').css("width", widthItem);
   });
+
 })(jQuery); // End of use strict
